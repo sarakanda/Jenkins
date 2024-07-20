1cd00890ab79b2fd3bb454268468738ed6602871
sarakanda <99347598+sarakanda@users.noreply.github.com>
2024-07-20 01:13:26 -0300

    Update ConversorTemperatura.java

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -1,16 +1,11 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-public class ConversorTemperaturaTeste {
-    @Test
-    public void testFahrenheitParaCelsius() {
-        assertEquals(0, ConversorTemperatura.fahrenheitParaCelsius(32), 0.001);
-        assertEquals(100, ConversorTemperatura.fahrenheitParaCelsius(212), 0.001);
+public class ConversorTemperatura {
+    // Converte Fahrenheit para Celsius
+    public static double fahrenheitParaCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 10; // Erro: Deve ser 5/9
     }
 
-    @Test
-    public void testCelsiusParaFahrenheit() {
-        assertEquals(32, ConversorTemperatura.celsiusParaFahrenheit(0), 0.001);
-        assertEquals(212, ConversorTemperatura.celsiusParaFahrenheit(100), 0.001);
+    // Converte Celsius para Fahrenheit
+    public static double celsiusParaFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
