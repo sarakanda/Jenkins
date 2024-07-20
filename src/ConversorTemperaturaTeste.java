45183d28f973a75b0a578efbcbb8bd9aabadd4a8
sarakanda <99347598+sarakanda@users.noreply.github.com>
2024-07-20 00:01:57 -0300

    Add files via upload

---
+++ src/ConversorTemperaturaTeste.java
@@ -0,0 +1,22 @@
+public class ConversorTemperaturaTeste {
+    public static void main(String[] args) {
+        testeFahrenheitParaCelsius();
+        testeCelsiusParaFahrenheit();
+    }
+
+    public static void testeFahrenheitParaCelsius() {
+        double result = ConversorTemperatura.fahrenheitParaCelsius(32);
+        System.out.println("32°F to Celsius: " + result); // Esperado: 0.0
+
+        result = ConversorTemperatura.fahrenheitParaCelsius(212);
+        System.out.println("212°F to Celsius: " + result); // Esperado: 100.0
+    }
+
+    public static void testeCelsiusParaFahrenheit() {
+        double result = ConversorTemperatura.celsiusParaFahrenheit(0);
+        System.out.println("0°C to Fahrenheit: " + result); // Esperado: 32.0
+
+        result = ConversorTemperatura.celsiusParaFahrenheit(100);
+        System.out.println("100°C to Fahrenheit: " + result); // Esperado: 212.0
+    }
+}
