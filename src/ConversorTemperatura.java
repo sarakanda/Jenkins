8ac78d3a7a1280a3f74b10e35e8c99d610b88995
sarakanda <99347598+sarakanda@users.noreply.github.com>
2024-07-20 01:02:05 -0300

    Update ConversorTemperatura.java

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -1,11 +1,11 @@
-public class ConversorTemperaturaaaa {
+public class ConversorTemperatura {
     // Converte Fahrenheit para Celsius
-    public static double fahrenheitParaCelsius(double fahrenheit) {
+    public static double fahrenheitParaCelsius( fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
 
     // Converte Celsius para Fahrenheit
-    public static double celsiusParaFahrenheit(double celsius) {
+    public static double celsiusParaFahrenheit( celsius) {
         return (celsius * 9 / 5) + 32;
     }
 }
